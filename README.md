b868d5457eb28cba2587af5941f6124a60a2498e
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2023-06-05 17:49:57 +0700

    remove gitignore files

--- README.md
+++
@@ -1,4 +0,0 @@
-# MDM_Project
-
-"cd back_end" then "npm install"
-npm run server
--- back_end/routes/activitiesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express =require('express')
-const router = express.Router()
-const activitiesController = require('../controllers/activitiesController')
-
-router.get('/',activitiesController.getAllActivities)
-
-module.exports = router
