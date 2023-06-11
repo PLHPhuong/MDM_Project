1716d4b880dbd75272af1f294ae9ae3da368be11
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2023-06-11 20:54:06 +0700

    ok login frontend

--- front_end/routes/unityRoute.js
+++ front_end/routes/unityRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios")
 // const unityController = require('../controllers/unityController')
 
 router.get("/", (req, res) => {
@@ -9,8 +10,20 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/login", (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
+
 router.get("/404", (req, res) => {
   res.render("404", {
+    layout: false,
+  });
+});
+
+router.get("/test", (req, res) => {
+  res.render("test", {
     layout: false,
   });
 });
