9ba86714b284d0bc6551d98407549d3b6ca818ee
Hoàng Huy <hoanghuy150199@gmail.com>
2023-07-08 10:27:35 +0700

    modify test1.hbs & app.js

--- front_end/app.js
+++ front_end/app.js
@@ -1,55 +1,52 @@
-const express = require('express');
+const express = require("express");
 const colors = require("colors");
-const dotenv = require("dotenv").config({path: "../.env"});
-const exphbs = require('express-handlebars');
+const dotenv = require("dotenv").config({ path: "../.env" });
+const exphbs = require("express-handlebars");
 const path = require("path");
-const { engine } = require('express-handlebars');
-const cookieParser = require('cookie-parser');
-const session = require('express-session')
-const port = process.env.FRONTEND_PORT || 3000
+const { engine } = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const port = process.env.FRONTEND_PORT || 3000;
 
 const app = express();
 
 // Use static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-
-
 const hbs = exphbs.create({
-    // defaultLayout:false,
-    extname:'hbs'
+  // defaultLayout:false,
+  extname: "hbs",
 });
-
-
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false,}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(session({
-    secret: 'group10',
+app.use(
+  session({
+    secret: "group10",
     resave: true,
     saveUninitialized: true,
     cookie: {
-        expires: 600000
-    }
-}));
+      expires: 600000,
+    },
+  })
+);
 
 app.use((req, res, next) => {
-    if (req.cookies.user && !req.session.user) {
-        res.clearCookie('user');        
-    }
-    next();
+  if (req.cookies.user && !req.session.user) {
+    res.clearCookie("user");
+  }
+  next();
 });
-
 
 // app.use("/", (req,res) => {
 //     res.render("temp", {
-//         layout: 'main' 
+//         layout: 'main'
 //     });
 // });
 
@@ -66,12 +63,9 @@ app.use("/attraction", require("./routes/attractionRoute"));
 
 // app.set("view engine", 'hbs');
 
-
 // app.get('/temp',(req,res)=> {
 //     res.render('temp')
 // })
-
-
 
 // // Use static folder
 // app.use(express.static(path.join(__dirname, "public")));
@@ -89,4 +83,3 @@ app.use("/attraction", require("./routes/attractionRoute"));
 // app.use("/", require("./routes/unityRoute"));
 
 app.listen(port, () => console.log(`Client start on port ${port}`));
-
